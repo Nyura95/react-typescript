0c6d039e923ca1da20262580f61a1569fe1d7cfe
Nyura95 <taxisoopya@hotmail.fr>
2018-09-24 19:45:26 +0200

    update action

--- src/actions/Counter.ts
+++ src/actions/Counter.ts
@@ -8,20 +8,22 @@ import { State as StateCounter } from '../reducers/Counter';
 import { ADD_COUNTER, SET_COUNTER, Action } from './';
 
 // Action increment the counter
-export const setCounter = (counter: number) => ({
-  type: ADD_COUNTER,
-  payload: {
-    counter,
-  },
-});
+export const setCounter = (counter: number) => (dispatch: Dispatch<Action<StateCounter>>) =>
+  dispatch({
+    type: ADD_COUNTER,
+    payload: {
+      counter,
+    },
+  });
 
 // Action for reset the counter from 0
-export const resetCounter = () => ({
-  type: SET_COUNTER,
-  payload: {
-    counter: 0,
-  },
-});
+export const resetCounter = () => (dispatch: Dispatch<Action<StateCounter>>) =>
+  dispatch({
+    type: SET_COUNTER,
+    payload: {
+      counter: 0,
+    },
+  });
 
 // Action decrement the counter
 export const AsyncSetCounter = (counter: number) => (dispatch: Dispatch<Action<StateCounter>>) => {
