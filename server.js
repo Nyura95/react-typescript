a87f5787dd4a98656b39725d37965f9c596bca13
Nyura95 <taxisoopya@hotmail.fr>
2018-11-08 18:38:28 +0100

    update build

--- server.js
+++ server.js
@@ -7,13 +7,11 @@ const PORT = process.argv[2];
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('*', function(_, response) {
-  response.sendFile(__dirname + '/dist/index.html');
+  response.sendFile(__dirname + '/build/index.html');
 });
 
-app.listen(
-  PORT,
-  error =>
-    error
-      ? console.error(error)
-      : console.info(`Listening on port ${PORT}. Visit http://localhost:${PORT}/ in your browser.`)
+app.listen(PORT, error =>
+  error
+    ? console.error(error)
+    : console.info(`Listening on port ${PORT}. Visit http://localhost:${PORT}/ in your browser.`)
 );
