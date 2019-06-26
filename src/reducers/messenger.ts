7fd5da3368d017f6b395468a4a32c52453e39aa1
Alexis Courapied <acourapied@enovee.eu>
2019-06-26 18:54:49 +0200

    nop

---
+++ src/reducers/messenger.ts
@@ -0,0 +1,39 @@
+import { IAction } from '../actions';
+
+export type ILoadScreenType = '';
+
+export type ILoadScreenState = {
+  show: boolean;
+  text: string;
+};
+export type ILoadScreenAction = IAction<ILoadScreenState, ILoadScreenType>;
+export type ILoadScreenDispatch = IAction<ILoadScreenState, ILoadScreenType>;
+
+const initialState: ILoadScreenState = {
+  show: false,
+  text: ''
+};
+
+/**
+ * Store for the module loadscreen
+ * @param {ILoadScreenState} state
+ * @param {IAction<ILoadScreenState, IType>} action
+ * @version 1.0.0
+ * @returns {ILoadScreenState}
+ */
+export function loadscreen(state = initialState, action: IAction<ILoadScreenState, ILoadScreenType>): ILoadScreenState {
+  switch (action.type) {
+    case 'SHOW_LOADSCREEN':
+      return {
+        text: action.payload.text || state.text,
+        show: true
+      };
+    case 'HIDE_LOADSCREEN':
+      return {
+        ...state,
+        show: false
+      };
+    default:
+      return state;
+  }
+}
--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -2,22 +2,62 @@ import * as React from 'react';
 
 // modules
 import { Row, Col } from 'reactstrap';
-import { useSelector } from 'react-redux';
-import { I18n } from 'react-redux-i18n';
 import { RouteComponentProps } from 'react-router';
 
 // style
 import * as styles from './styles.scss';
 
-import { IReduxState, IUserState } from '../../reducers';
+import { Button, Input } from '../../components';
+
+const uri = 'ws://127.0.0.1:3001';
 
 const Home: IHook<RouteComponentProps> = () => {
-  const { username } = useSelector<IReduxState, IUserState>(reducer => reducer.user);
+  const [messages, setMessages] = React.useState<string[]>([]); // http://localhost:3000/api/v1/conversation/18
+  const [message, setMessage] = React.useState<string>('');
+
+  const webSocket = React.useMemo(() => {
+    const webSocket = new WebSocket(uri);
+    webSocket.onopen = () => {
+      webSocket.send(
+        JSON.stringify({
+          token: 'vTMGcVAE5EBwxqAVv1SPvfs_tao=',
+          id: 2588,
+          action: 'onload'
+        })
+      );
+    };
+
+    webSocket.onmessage = event => {
+      setMessages([...messages, event.data]);
+    };
+
+    return webSocket;
+  }, []);
+
+  React.useEffect(() => {
+    return () => {
+      webSocket.close();
+    };
+  }, []);
+
+  const sendMessage = React.useCallback(
+    (message: string) => () => {
+      webSocket.send(
+        JSON.stringify({
+          action: 'send',
+          message: message
+        })
+      );
+      setMessage('');
+    },
+    []
+  );
 
   return (
     <Row className={styles.container}>
       <Col lg="12" className={styles.container_button}>
-        {I18n.t('pages.home.hello', { username })}
+        <Input type="text" value={message} onChange={message => setMessage(message)} />
+        <Button.Rectangle onClick={sendMessage(message)}>Envoyer</Button.Rectangle>
       </Col>
     </Row>
   );
