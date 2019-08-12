9878dcb43530e306d4b54fef83f0e07fef09daf0
Alexis Courapied <acourapied@enovee.eu>
2019-08-12 14:53:01 +0200

    remove old type

--- src/actions/types.ts
+++ src/actions/types.ts
@@ -1,13 +1,6 @@
-import { ThunkAction, ThunkDispatch } from 'redux-thunk';
-import { IReduxState } from '../reducers';
-
 // my interface Action
 export interface IAction<S, T, SAGA = {}> {
   type: T;
   payload: Partial<S>;
   saga?: SAGA;
 }
-
-// thunk (async dispatch/action)
-export type ICustomDispatch<S, T> = ThunkDispatch<IReduxState, null, IAction<S, T>>;
-export type ICustomAction<S, T> = ThunkAction<void, IReduxState, null, IAction<S, T>>;
