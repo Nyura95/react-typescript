c62e4b3f4eba1e9bd960d013e425699e9a11a6ba
Alexis Courapied <acourapied@enovee.eu>
2019-10-25 15:04:15 +0200

    add com

--- jest.setup.js
+++ jest.setup.js
@@ -1,6 +1,7 @@
 const { configure } = require('enzyme');
 const Adapter = require('enzyme-adapter-react-16');
 
+// require for mock the translation
 const i18n = require('i18next');
 const { initReactI18next } = require('react-i18next');
 
