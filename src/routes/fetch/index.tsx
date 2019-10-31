a36d28c10011d3a2bfd4639dc43234fc7a8bee1d
Alexis Courapied <acourapied@enovee.eu>
2019-10-31 18:36:33 +0100

    update swr

--- src/routes/fetch/index.tsx
+++ src/routes/fetch/index.tsx
@@ -6,13 +6,15 @@ import useSWR from 'swr';
 const getMessages = () => {
   return new Promise((resolve, reject) => {
     console.log('fetch');
-    resolve('ok');
+    setTimeout(() => {
+      resolve('ok');
+    }, 1000);
   });
 };
 
 function Profile() {
   const { data } = useSWR<string>('/api/user', getMessages, { suspense: true });
-  return <div>hello, {data}</div>;
+  return <div>{data}</div>;
 }
 
 function App() {
